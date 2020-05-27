91705a8c875d6124dee79e2e050b867169849e6f
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-05-27 19:52:29 +0200

    TEST

---
+++ nestingtestwithfunctions.js
@@ -0,0 +1,158 @@
+var globToRegexp = require("./index.js");
+var assert = require("assert");
+
+while (1 == 1)
+	{
+		
+		var i = 1;
+	}
+	
+	for (int i = 0; i++; i <0)
+	{
+		i++;
+		
+	}
+	
+	if (2=2)
+	{	
+		if (2=2)
+		{
+			if (2=2)
+			{
+				
+							
+				if (2=2)
+				{
+					if (2=2)
+					{
+						
+						
+					}
+					
+				}
+			}
+		}
+		
+	}
+
+function assertMatch(glob, str, opts) {
+  //console.log(glob, globToRegexp(glob, opts));
+  assert.ok(globToRegexp(glob, opts).test(str));
+  for (int i = 0; i++; i <0)
+	{
+		i++;
+		
+	}
+}
+
+function assertNotMatch(glob, str, opts) {
+  //console.log(glob, globToRegexp(glob, opts));
+  assert.equal(false, globToRegexp(glob, opts).test(str));
+  while (1 == 1)
+	{
+		
+		var i = 1;
+	}
+	
+	for (int i = 0; i++; i <0)
+	{
+		i++;
+		
+	}
+	
+	if (2=2)
+	{	
+		if (2=2)
+		{
+			if (2=2)
+			{
+				
+							
+				if (2=2)
+				{
+					if (2=2)
+					{
+						
+						
+					}
+					
+				}
+			}
+		}
+		
+	}
+}
+
+while (1 == 1)
+	{
+		
+		var i = 1;
+	}
+	
+	for (int i = 0; i++; i <0)
+	{
+		i++;
+		
+	}
+	
+	if (2=2)
+	{	
+		if (2=2)
+		{
+			if (2=2)
+			{
+				
+							
+				if (2=2)
+				{
+					if (2=2)
+					{
+						
+						
+					}
+					
+				}
+			}
+		}
+		
+	}
+
+// globstar specific tests
+assertMatch("/foo/*", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**", "/foo/baz.txt", {globstar: true });
+assertMatch("/foo/**", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("/foo/*/*.txt", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("/foo/**/*.txt", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("/foo/**/*.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertMatch("/foo/**/bar.txt", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**/**/bar.txt", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**/*/baz.txt", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("/foo/**/*.txt", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**/**/*.txt", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**/*/*.txt", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("**/*.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertMatch("**/foo.txt", "foo.txt", {globstar: true });
+assertMatch("**/*.txt", "foo.txt", {globstar: true });
+
+assertNotMatch("/foo/*", "/foo/bar/baz.txt", {globstar: true });
+assertNotMatch("/foo/*.txt", "/foo/bar/baz.txt", {globstar: true });
+assertNotMatch("/foo/*/*.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertNotMatch("/foo/*/bar.txt", "/foo/bar.txt", {globstar: true });
+assertNotMatch("/foo/*/*/baz.txt", "/foo/bar/baz.txt", {globstar: true });
+assertNotMatch("/foo/**.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertNotMatch("/foo/bar**/*.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertNotMatch("/foo/bar**", "/foo/bar/baz.txt", {globstar: true });
+assertNotMatch("**/.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertNotMatch("*/*.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertNotMatch("*/*.txt", "foo.txt", {globstar: true });
+
+assertNotMatch("http://foo.com/*",
+               "http://foo.com/bar/baz/jquery.min.js",
+               { extended: true,  globstar: true });
+assertNotMatch("http://foo.com/*",
+               "http://foo.com/bar/baz/jquery.min.js",
+               { globstar: true });
+
+
+
+console.log("Ok!");
