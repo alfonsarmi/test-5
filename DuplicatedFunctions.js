92cb7a0fb2cf85308b6bf982e696174fa117f9e3
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-06-05 13:10:41 +0200

    asdfas

---
+++ DuplicatedFunctions.js
@@ -0,0 +1,111 @@
+var globToRegexp = require("./index.js");
+var assert = require("assert");
+
+while (1 == 1)
+	{
+		
+		var i = 1;
+	}
+	
+for (int i = 0; i++; i <0)
+{
+	i++;
+	
+}
+
+if (2=2)
+{	
+	if (2=2)
+	{
+		if (2=2)
+		{
+			
+						
+			if (2=2)
+			{
+				if (2=2)
+				{
+					
+					
+				}
+				
+			}
+		}
+	}
+	
+}
+
+function assertMatch(glob, str, opts) {
+  //console.log(glob, globToRegexp(glob, opts));
+  assert.ok(globToRegexp(glob, opts).test(str));
+  for (int i = 0; i++; i <0)
+	{
+		i++;
+		
+	}
+}
+
+function assertMatch(glob, opts) {
+  //console.log(glob, globToRegexp(glob, opts));
+  //assert.ok(globToRegexp(glob, opts).test(str));
+  for (int i = 0; i++; i <0)
+	{
+		i++;
+		
+	}
+}
+
+
+while (1 == 1)
+	{
+		
+		var i = 1;
+	}
+	
+for (int i = 0; i++; i <0)
+{
+	i++;
+	
+}
+
+if (2=2)
+{	
+	if (2=2)
+	{
+		if (2=2)
+		{
+			
+						
+			if (2=2)
+			{
+				if (2=2)
+				{
+					
+					
+				}
+				
+			}
+		}
+	}
+	
+}
+
+// globstar specific tests
+assertMatch("/foo/*", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/*", {globstar: true });
+assertMatch("/foo/**", "/foo/baz.txt", {globstar: true });
+assertMatch("/foo/**", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("/foo/*/*.txt", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("/foo/**/*.txt", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("/foo/**/*.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertMatch("/foo/**/bar.txt", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**/**/bar.txt", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**/*/baz.txt", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("/foo/**/*.txt", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**/**/*.txt", "/foo/bar.txt", {globstar: true });
+assertMatch("/foo/**/*/*.txt", "/foo/bar/baz.txt", {globstar: true });
+assertMatch("**/*.txt", "/foo/bar/baz/qux.txt", {globstar: true });
+assertMatch("**/foo.txt", "foo.txt", {globstar: true });
+assertMatch("**/*.txt", "foo.txt", {globstar: true });
+
+console.log("Ok!");
